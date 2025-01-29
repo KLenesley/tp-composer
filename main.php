9633e4390ff99b996f3d11a831f76e9849bee6d0
KLenesley <lenesley.kylian@ik.me>
2025-01-29 16:07:40 +0100

    Connexion à la BD

--- main.php
+++ main.php
@@ -8,6 +8,13 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 use Symfony\Component\Dotenv\Dotenv;
 
+function AddFighter($l)
+{
+
+    $l->info('Fighter Successfully added');
+
+}
+
 // Premiers pas avec Composer
 print("Premiers pas avec Composer \n \n");
 
@@ -20,29 +27,7 @@ $dotenv = new Dotenv();
 $dotenv->load(__DIR__.'/.env');
 
 $log->info('Debut du programme');
-$mail = new PHPMailer(true);
 
-try {
-    $mail = new PHPMailer(true);
+AddFighter($log);
 
-    $mail->isSMTP();
-    $mail->Host = $_ENV['SMTP_HOST'];
-    $mail->SMTPAuth = true;
-    $mail->Username = $_ENV['SMTP_USERNAME'];
-    $mail->Password = $_ENV['SMTP_PASSWORD'];
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-    $mail->Port = 465;
-
-    $mail->setFrom($_ENV['SMTP_USERNAME'], $_ENV['SMTP_NAME']);
-    $mail->addAddress($_ENV['SMTP_USERNAME'], $_ENV['SMTP_NAME']);
-    $mail->Subject = 'TEST';
-    $mail->Body    = 'Test D\'envoi de mail avec PHPMailer';
-
-    $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    $log->error('Le message n\'a pas pu être envoyer: ', [$e->getMessage()]);
-}
-
-$log->info('Mail envoyé');
 ?>
